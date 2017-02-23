6b91a5711046a76b946360a308c80cfb64e700ec
Kassam Mustapha <mustaphakassam@gmail.com>
2017-02-23 10:51:56 +0300

    initial

---
+++ g3e-hditOne.cpp
@@ -0,0 +1,100 @@
+//program for SIMUPESA
+#include<iostream>
+using namespace std;
+int main (){
+	
+	//declaration of variables
+	// variable "userMainChoice" it will store entered user choice
+	// variable "subMenuChoice" it will store entered user choice from sub menu
+	int userMainChoice, subMenuChoice;;
+	//output the choice to the user in "SIMUPESA" application
+	cout<<"KARIBU KWENYE HUDUMA YA SIMUPESA\n"<<"CHAGUA\n";
+	cout<<"1-> KUTOA PESA\n"<<"2-> KUWEKA PESA\n"<<"3-> KUTUMA PESA\n"<<"4-> KUNUNUA MUDA WA MAONGEZI\n"<<"5-> KULIPA BILI\n"
+	<<"6-> KUNUNUA KIFURUSHI\n"<<"7-> KUKOPA\n\n"<<"CHAGUO LAKO NI:  ";
+	//allow user to enter the choice
+	returnToEnterChoice:cin>> userMainChoice;
+	cout<<endl;
+	
+	//checking the entered menu choice
+	switch(userMainChoice)
+	{
+		case 1:
+			cout << "under construction....\n";
+			break;
+		case 2:
+			cout << "under construction....\n";
+			break;
+		case 3:
+			cout << "under construction....\n";
+			break;
+		case 4:
+			cout << "under construction....\n";
+			break;
+		case 5:
+			cout<<"KARIBU KWENYE HUDUMA YA KULIPIA BILI\n"<<"CHAGUA HUDUMA\n";
+			cout<<"1-> TANESCO - LUKU\n"<<"2-> DAWASCO - MAJI\n"<<"3-> POLISI - FAINI\n"
+			<<"4-> MWENDOKASI - NAULI\n"<<"CHAGUO LANGU NI:  ";
+			
+			//allow user to repeate the choice again
+			returnToEnterSubMenuChoice: cin>> subMenuChoice;
+			cout<<endl;
+			//checking the entered sub menu choice
+			switch(subMenuChoice)
+			{
+				case 1:
+					cout << "under construction....\n";
+					break;
+				case 2:
+					//declaration of variables for storing user/customer 
+					int userBalance, userAmmount;
+					//initialize 3400 balance for user/customer
+					userBalance = 3400;
+					cout<<"KARIBU DAWASCO ULIPE BILI YA MAJI\n";
+					enterAmount:cout<<"Ingiza kiasi cha pesa (TSH):  ";
+					cin >> userAmmount;
+					//checking if the purchased amount is greater than or equal to 305.00
+					if(userAmmount >= 305.00)
+					{
+						cout<<"ndio hapa";
+
+					}else{
+						//displaying the Error message to the user interface
+						cout<<"SAMAHANI, HUWEZI KULIPA CHINI YA TSH 305.00\n";
+						goto enterAmount;
+					};
+					
+					break;
+				case 3:
+					cout << "under construction....\n";
+					break;
+					cout << "under construction....\n";
+				case 4:
+					cout << "under construction....\n";
+					break;
+				default:
+					cout<<"SAMAHANI, TUMESHINDWA KUITAMBUA HUDUMA\n"<<endl<<"CHAGUO LANGU NI : ";
+					//allow user to enter another choice from the sub menu
+					goto returnToEnterSubMenuChoice;
+					break;
+				
+			}
+			cout<<endl;
+			break;
+		case 6:
+			cout << "under construction....\n";
+			break;
+		case 7:
+			cout << "under construction....\n";
+		default:
+			//Error message that will to the user if is entered wrong choice
+			cout<<"CHAGUO HALIJATAMBULIKA, SAMAHANI RUDIA TENA\n"<<"CHAGUO LANGU NI:  ";
+			//ask again user to enter another choice from the main menu
+			goto returnToEnterChoice;
+			
+			break;
+			
+		
+	}
+	
+	
+}
