33f6e0bd29fdb5b8be7430de6022464cf91c404f
kassam-binary <mustaphakassam@gmail.com>
2017-02-24 15:03:44 +0300

    finilizing for calculating new balance nad setting decimal place

--- g3e-hditOne.cpp
+++ g3e-hditOne.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 //declaration VAT calculating function "prototype function"
-float vatCalculating(float amount,float charge);
+float vatCalculating(float amount);
 int main (){
-	
+
 	//declaration of variables
 	int userMainChoice, subMenuChoice;;
 	//output the choice to the user in "SIMUPESA" application
@@ -34,7 +34,7 @@ int main (){
 			cout<<"KARIBU KWENYE HUDUMA YA KULIPIA BILI\n"<<"CHAGUA HUDUMA\n";
 			cout<<"1-> TANESCO - LUKU\n"<<"2-> DAWASCO - MAJI\n"<<"3-> POLISI - FAINI\n"
 			<<"4-> MWENDOKASI - NAULI\n"<<"CHAGUO LANGU NI:  ";
-			
+
 			//allow user to repeate the choice again
 			returnToEnterSubMenuChoice: cin>> subMenuChoice;
 			cout<<endl;
@@ -45,30 +45,34 @@ int main (){
 					cout << "under construction....\n";
 					break;
 				case 2:
-					//declaration of variables for storing user/customer 
+					//declaration of variables for storing user/customer balance
 					float userBalance;
-					float userAmmount;
+					float userAmount;
 					//initialize 3400 balance for user/customer
 					userBalance = 3400;
 					cout<<"KARIBU DAWASCO ULIPE BILI YA MAJI\n";
 					enterAmount:cout<<"Ingiza kiasi cha pesa (TSH):  ";
-					cin >> userAmmount;
+					cin >> userAmount;
 					cout<<endl;
 					//checking if the purchased amount is greater than or equal to 305.00
-					if(userAmmount >= 305.00)
+					if(userAmount >= 305.00)
 					{
-						if(userAmmount >= userBalance )
+						if(userAmount >= userBalance )
 						{
 							//displaying Error message to the if entered greater amount than balance
-							cout<<"SAMAHANI, HUWEZI KULIPA MAJI YA TSH "<<userAmmount<<"\nSALIO LAKO HALITOSHELEZI\n";
+							cout<<"SAMAHANI, HUWEZI KULIPA MAJI YA TSH "<<userAmount<<"\nSALIO LAKO HALITOSHELEZI\n";
 							cout<<"SALIO LAKO NI TSH "<<userBalance<<endl<<endl;
 							//allow user to enter again amount
 							goto enterAmount;
 						}else{
 							//cout<<"ndio hapa";
 							float fixedServiceCharge = 205;
-							cout<<"HUDUMA IMEKAMILIKA\n"<<"GHARAMA YA VAT NI TSH "<<vatCalculating (userAmmount, fixedServiceCharge)<<endl;
+							float newBalance = userAmount + fixedServiceCharge + vatCalculating(userAmount);
+							
+							cout<<"HUDUMA IMEKAMILIKA\n"<<"UMEFANIKIWA KULIPA MAJI YA TSH "<<userAmount<<endl;
+							cout<<"GHARAMA YA VAT NI TSH "<<fixed<<setprecision(2)<<vatCalculating(userAmount)<<endl;
 							cout<<"GHARAMA YA SERVICE CHARGE NI TSH "<<fixedServiceCharge<<endl;
+							cout<<"SALIO LAKO JIPYA NI TSH "<<fixed<<setprecision(2)<<userBalance - newBalance<<endl;
 							cout<<"AHSANTE KWA KUTUMIA HUDUMA YETU";
 						}
 
@@ -77,7 +81,7 @@ int main (){
 						cout<<"SAMAHANI, HUWEZI KULIPA CHINI YA TSH 305.00\n";
 						goto enterAmount;
 					};
-					
+
 					break;
 				case 3:
 					cout << "under construction....\n";
@@ -91,7 +95,7 @@ int main (){
 					//allow user to enter another choice from the sub menu
 					goto returnToEnterSubMenuChoice;
 					break;
-				
+
 			}
 			cout<<endl;
 			break;
@@ -100,27 +104,28 @@ int main (){
 			break;
 		case 7:
 			cout << "under construction....\n";
+			break;
 		default:
 			//Error message that will to the user if is entered wrong choice
 			cout<<"CHAGUO HALIJATAMBULIKA, SAMAHANI RUDIA TENA\n"<<"CHAGUO LANGU NI:  ";
 			//ask again user to enter another choice from the main menu
 			goto returnToEnterChoice;
-			
+
 			break;
-			
-		
+
+
 	}
-	
+
 }
 
 //body of VAT function
- float vatCalculating (float amount,float charge){
- 	float addVatPercentage, vatAmmount;
+ float vatCalculating (float amount){
+ 	float addVatPercentage;
  	float vat = 0.18;
  	addVatPercentage = amount * vat;
  	//adding fixed service charge which is 205
  	//vatAmmount = addVatPercentage + charge;
- 	
+
  	//return vat with it's charge added
  	return addVatPercentage;
  }
